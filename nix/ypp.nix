73e483c642ea84a4fb0fd633bb5cb618d9f965eb
Akira Yamazaki <chihsiao@outlook.com>
2024-12-14 17:30:00 +0800

    chore: update dependencies

--- nix/ypp.nix
+++ nix/ypp.nix
@@ -5,15 +5,15 @@
 }:
 
 let
-  url = "https://cdelord.fr/hey/ypp-linux-x86_64.tar.xz";
-  hash = "sha256-CngKUXs3i/ca9D88LYyGGguW+JZ4WzDq0WDvcAoIp9U=";
-  lastUpdate = "2024-04-12";
+  url = "https://cdelord.fr/pub/luax-ssl-linux-x86_64.tar.xz";
+  hash = "sha256-DSTgoL12v8Rox/cTz1ksUNp6I5JWQTg3KV8mWU6kPIY=";
+  lastUpdate = "2024-12-14";
 in
 
 stdenv.mkDerivation {
   pname = "ypp";
   version = lastUpdate;
-  src = fetchzip { inherit url hash; };
+  src = fetchzip { inherit url hash; stripRoot = false; };
 
   nativeBuildInputs = [
     autoPatchelfHook
