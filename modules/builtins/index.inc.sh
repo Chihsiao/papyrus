91f7b9f27dd7a1122ad0930ca016b3e3dbb93ad4
Akira Yamazaki <chihsiao@outlook.com>
2025-05-29 22:00:00 +0800

    refactor(m:builtins): reorganize lua files into dedicated directory

--- modules/builtins/index.inc.sh
+++ modules/builtins/index.inc.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=bash
 # shellcheck disable=SC2154,SC1091
 
-PAPYRUS_YPP_FLAGS+=(-l "$__MODULE_ROOT__/utils.lua")
+PAPYRUS_YPP_FLAGS+=(-l "$__MODULE_ROOT__/lua/utils.lua")
 
 source "$__MODULE_ROOT__/metadata.inc.sh"
 source "$__MODULE_ROOT__/refdoc.inc.sh"
